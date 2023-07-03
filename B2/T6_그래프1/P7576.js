f2714aef3b044724e44ccf27f23fb5f6bbcf7474
sunhwa <sumz.ko@gmail.com>
2023-07-03 20:35:01 +0900

    [골드 5] 토마토

--- B2/T6_그래프1/P7576.js
+++ B2/T6_그래프1/P7576.js
@@ -2,12 +2,12 @@
 // 토마토
 // 골드 5
 
-// 메모리초과
+// 메모리초과 -- 해결
 // max 값 계산해줄 필요 없이 day 계산으로 구할 수 있음
 // checkFlag를 두어서 해당 일에 익은 토마토 없으면 반복문 종료
 
-const NOT_VISIT = 9000001;
-const EMPTY = 9000000;
+const NOT_VISIT = 0;
+const EMPTY = -1;
 
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
@@ -26,7 +26,7 @@ if (!box.flat().includes(1)) {
   return;
 } else if (!box.flat().includes(0)) {
   // 익을 토마토가 없음 & 이미 다익음
-  console.log(1);
+  console.log(0);
   return;
 }
 
@@ -37,85 +37,53 @@ function findFastestDays({ width, height, map }) {
     Array(width).fill(NOT_VISIT)
   );
 
-  // 익어있는 토마토 목록
-  const locationOfRipeTomatoes = [];
+  let verticesToVisit = [];
   for (let i = 0; i < height; i += 1) {
     for (let j = 0; j < width; j += 1) {
       if (map[i][j] === 1) {
-        locationOfRipeTomatoes.push([i, j]);
+        verticesToVisit.push([i, j]);
         check[i][j] = 1;
       } else if (map[i][j] === -1) {
         check[i][j] = EMPTY;
       }
     }
   }
-  // console.log(locationOfRipeTomatoes, check);
-
-  // 익은 토마토 위치들 주변 위치들 verticesToVisit에 넣어서 초기화
-  let verticesToVisit = locationOfRipeTomatoes.map(
-    ([curRowIdx, curColumnIdx]) => [
-      ...createMovablePoints({
-        height,
-        width,
-        curRowIdx,
-        curColumnIdx,
-      }),
-    ]
-  );
-  verticesToVisit = verticesToVisit.flat();
   // console.log(verticesToVisit);
 
-  let curI = 0;
-  let curJ = 0;
+  let day = 1;
 
+  let checkFlag = false;
   while (verticesToVisit.length > 0) {
-    [[curI, curJ], ...verticesToVisit] = verticesToVisit;
-    // console.log("cur", [curI, curJ]);
+    // console.log(`[[${day}]] `, verticesToVisit);
+    let verticesToAdd = [];
+    checkFlag = false;
 
-    if (check[curI][curJ] !== NOT_VISIT) continue;
-    if (map[curI][curJ] === -1) {
-      check[curI][curJ] = EMPTY;
-      continue;
+    for (let [curI, curJ] of verticesToVisit) {
+      const points = createMovablePoints({
+        height,
+        width,
+        curRowIdx: curI,
+        curColumnIdx: curJ,
+      });
+
+      for (let [nextI, nextJ] of points) {
+        if (check[nextI][nextJ] !== 0) continue;
+        checkFlag = true;
+        check[nextI][nextJ] = day + 1;
+        verticesToAdd.push([nextI, nextJ]);
+      }
     }
-
-    const points = createMovablePoints({
-      height,
-      width,
-      curRowIdx: curI,
-      curColumnIdx: curJ,
-    });
-
-    const values = points
-      .map(([i, j]) => check[i][j])
-      .filter((v) => v !== NOT_VISIT && v !== EMPTY);
-    // console.log(verticesToVisit);
-    // console.log(values);
-    // console.log(check);
-    const min = Math.min(...values);
-
-    check[curI][curJ] = min + 1;
-    verticesToVisit = [
-      ...verticesToVisit,
-      // ...points
-      ...points.filter(([i, j]) => map[i][j] !== -1),
-    ];
+    // console.log(`[${day}] `, checkFlag, "check----", check);
+    if (!checkFlag) break;
+    verticesToVisit = [...verticesToAdd];
+    day += 1;
   }
 
-  // console.log(check);
-
-  const max = Math.max(...check.flat().filter((v) => v !== EMPTY));
-  if (max === NOT_VISIT) {
-    /** 갈 수 없는 곳이 존재하는 경우 처리하기 위함
-     * 6 4
-0 -1 0 0 0 0
--1 0 0 0 0 0
-0 0 0 0 0 0
-0 0 0 0 0 1
-     */
+  if (check.map((row) => row.filter((v) => v == 0)).flat().length) {
     console.log(-1);
     return;
   }
-  console.log(max - 1);
+  console.log(day - 1);
 }
 
 function createMovablePoints({
@@ -125,14 +93,10 @@ function createMovablePoints({
   curColumnIdx: j, // w
 }) {
   const MOVE_POINTS = [
-    // [-1, -1],
     [-1, 0],
-    // [-1, 1],
     [0, -1],
     [0, 1],
-    // [1, -1],
     [1, 0],
-    // [1, 1],
   ];
 
   return MOVE_POINTS.map(([mi, mj]) => [i + mi, j + mj]).filter(
