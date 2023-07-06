e0626e37efbd75ac1cc0d197753f50329cdad9eb
sunhwa <sumz.ko@gmail.com>
2023-07-06 12:11:02 +0900

    [골드4] Two Dots

--- B2/T6_그래프1/P16929.js
+++ B2/T6_그래프1/P16929.js
@@ -3,13 +3,18 @@
 // 골드 4
 
 // 문제를 잘못 이해핬다.
-// ㅁ가 아니라 그냥 연결했을 때 선이아닌 도형으로 이루어지기만 하는 것을 확인하는 것이 였다...
+// 꼭 네모가 아니라 선이아닌 도형으로 이루어지기만 하는 것을 확인하는 것이 였다...
 // 문제를 잘 읽어봐야겠다...
 
-const MODE = {
-  DOWN: 0,
-  RIGHT: 1,
-};
+const NOT_VISIT = 0;
+const VISIT = 1;
+
+const MOVE_POINTS = [
+  [[0, 1], 0],
+  [[1, 0], 1],
+  [[0, -1], 2],
+  [[-1, 0], 3],
+];
 
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
@@ -22,9 +27,10 @@ const board = Array.from({ length: H }).map((_, i) => input[i + 1].split(""));
 run();
 
 function run() {
-  for (let i = 0; i < H - 1; i++) {
-    for (let j = 0; j < W - 1; j++) {
-      if (check({ startI: i, startJ: j })) {
+  for (let i = 0; i < H; i++) {
+    for (let j = 0; j < W; j++) {
+      // console.log("######## [", i, j, "]", board[i][j]);
+      if (checkCycle({ startI: i, startJ: j })) {
         console.log("Yes");
         return;
       }
@@ -33,70 +39,73 @@ function run() {
   console.log("No");
 }
 
-function check({ startI, startJ }) {
-  let type = board[startI][startJ];
-  // console.log("####", startI, startJ, "-----", type);
+function checkCycle({ startI, startJ }) {
+  const type = board[startI][startJ];
+  const visited = Array.from({ length: H }).map((_) =>
+    Array(W).fill(NOT_VISIT)
+  );
 
-  let curI = startI;
-  let curJ = startJ + 1;
+  let [curI, curJ, curDir] = [startI, startJ, -1];
+  let pointsToCheck = [[[curI, curJ], -1]];
+  let cntMove = 0;
 
-  let targetI = curI + 1;
-  let targetJ = curJ;
+  while (pointsToCheck.length > 0) {
+    [[[curI, curJ], curDir], ...pointsToCheck] = pointsToCheck;
+    if (visited[curI][curJ] === VISIT) {
+      // console.log(" V [", curI, curJ, "]");
+      if (curI === startI && curJ === startJ && cntMove >= 4) return true;
+      continue;
+    }
 
-  let mode = MODE.DOWN; // 0: down, 1:right
+    visited[curI][curJ] = VISIT;
+    if (board[curI][curJ] !== type) continue;
+    if (curI < startI) continue;
 
-  while (curI < H && curJ < W) {
+    cntMove += 1;
+
+    const _Add = makeNextPoints(curI, curJ, curDir);
+    pointsToCheck = [..._Add, ...pointsToCheck];
+
     // console.log(
-    //   "[",
-    //   mode === 0 ? "D" : "R",
-    //   "]",
+    //   " - [",
     //   curI,
     //   curJ,
+    //   "]",
+    //   curDir,
     //   board[curI][curJ],
-    //   board[curI][curJ] === type
+    //   board[curI][curJ] === type,
+    //   _Add
     // );
-
-    if (board[curI][curJ] === type) {
-      if (mode === MODE.DOWN) {
-        if (curI < targetI - 1) {
-          curI += 1;
-        } else {
-          // curI === targetI
-          mode = MODE.RIGHT;
-          curI = targetI;
-          curJ = startJ;
-        }
-      } else {
-        // mode === MODE.RIGHT
-        if (curJ < targetJ) {
-          curJ += 1;
-        } else {
-          // curJ === targetJ
-          return true;
-        }
-      }
-    } else {
-      if (curI === startI || curJ === startJ) return false;
-
-      if (mode === MODE.DOWN) {
-        curI = 0;
-        curJ += 1;
-        targetJ = curJ;
-      } else {
-        // mode === MODE.RIGHT
-        if (curJ === targetJ) {
-          mode = MODE.DOWN;
-          targetI = curI + 1;
-          curJ = targetJ = curJ + 1;
-          curI = startI;
-        } else {
-          // curJ < targetJ
-          curJ = 0;
-          curI += 1;
-          targetI = curI;
-        }
-      }
-    }
   }
   return false;
 }
+
+function makeNextPoints(i, j, dir) {
+  let exceptDir = getExceptDir(dir);
+
+  return MOVE_POINTS.map(([[mi, mj], d]) => [[i + mi, j + mj], d]).filter(
+    ([[i, j], d]) => i >= 0 && i < H && j >= 0 && j < W && d !== exceptDir
+  );
+}
+
+// 온 방향으로 되돌아가지 않기 위함
+function getExceptDir(dir) {
+  switch (dir) {
+    case 0:
+      return 2;
+    case 1:
+      return 3;
+    case 2:
+      return 0;
+    case 3:
+      return 1;
+    default:
+      return -1;
+  }
+}
+
+// 추가 예제
+// 3 3
+// ABA
+// ABA
+// AAA
