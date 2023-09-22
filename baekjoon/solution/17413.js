17f3fdac4e88605f4bd72aeccd34f6dd4179567b
sunhwa <sumz.ko@gmail.com>
2023-09-22 14:56:37 +0900

    string / 실버3 / 20291 / 221008

---
+++ baekjoon/solution/17413.js
@@ -0,0 +1,40 @@
+// https://www.acmicpc.net/problem/17413
+// [이전기록] step - B1 T2_자료구조1 연습
+// 22/10/08
+// 단어 뒤집기 2
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input2.txt";
+
+let line = fs.readFileSync(filePath).toString().trim();
+
+function solution(line) {
+  let res = "";
+  let idx = 0;
+  let tmp = "";
+  while (idx < line.length) {
+    switch (line[idx]) {
+      case "<":
+        if (tmp != "") {
+          res += [...tmp].reverse().join("");
+          tmp = "";
+        }
+        let tagEndIdx = line.indexOf(">", idx);
+        res += line.substring(idx, tagEndIdx + 1);
+        idx = tagEndIdx + 1;
+        continue;
+      case " ":
+        res += [...tmp].reverse().join("") + " ";
+        tmp = "";
+        break;
+      default:
+        tmp += line[idx];
+        break;
+    }
+    idx++;
+  }
+  if (tmp != "") res += [...tmp].reverse().join("");
+  console.log(res);
+}
+
+solution(line);
